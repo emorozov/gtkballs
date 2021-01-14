efce62f43593c47ffdc2d2c005c6d1ccdf7a9204
wdlkmpx <wdlkmpx@gmail.com>
2021-01-14 21:03:50 +0800

    gfx.c/h:  consistent indentation

--- src/gfx.h
+++ src/gfx.h
@@ -2,14 +2,14 @@
 #define __GFX__H
 
 typedef enum {
-        DIR_LEFT,
-        DIR_RIGHT,
-        DIR_UP,
-        DIR_DOWN,
-        DIR_UP_LEFT,
-        DIR_DOWN_LEFT,
-        DIR_UP_RIGHT,
-        DIR_DOWN_RIGHT
+   DIR_LEFT,
+   DIR_RIGHT,
+   DIR_UP,
+   DIR_DOWN,
+   DIR_UP_LEFT,
+   DIR_DOWN_LEFT,
+   DIR_UP_RIGHT,
+   DIR_DOWN_RIGHT
 } Direction;
 
 void draw_next_balls(void);
